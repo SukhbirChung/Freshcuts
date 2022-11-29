72d59abeaa666a41e3420bfe7a4a23e365976106
SukhbirChung <sukhbirchung@gmail.com>
2022-11-29 14:31:34 -0500

    HTML & CSS finished for navbar.

---
+++ index.js
@@ -0,0 +1,6 @@
+/* Navbar */
+const menuIcon = document.querySelector('menuIcon');
+
+/* Set the current year in footer */
+let year = document.querySelector('footer span');
+year.textContent = new Date().getFullYear();
